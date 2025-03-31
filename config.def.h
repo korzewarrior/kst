1933889a4f7d31be3b4d9b37548d4c1420618f68
korze <korze84@gmail.com>
2025-03-31 08:40:49 -0700

    Use explicit red selection highlighting to match kitty/foot

--- config.def.h
+++ config.def.h
@@ -41,13 +41,14 @@ static const char *colorname[] = {
 	/* more colors can be added after 255 to use with DefaultXX */
 	"#d3d7cf", /* foreground */
 	"#000000", /* background */
+	"#cc0000", /* selection color - same as the red color */
 };
 
 /* Default colors (colorname index) */
 unsigned int defaultfg = 256;
 unsigned int defaultbg = 257;
-unsigned int defaultcs = 1;  /* Red for cursor/selection - matching kitty and foot */
-static unsigned int defaultrcs = 1;  /* Red for reverse cursor/selection */
+unsigned int defaultcs = 258;  /* Selection color - using our explicit red defined at index 258 */
+static unsigned int defaultrcs = 258;  /* Reverse selection color */
 
 /* Terminal settings */
 static char *shell = "/bin/sh";
